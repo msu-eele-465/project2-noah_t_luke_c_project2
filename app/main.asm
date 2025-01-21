7b91d70e9ce942fb13f950c62c850ecfba2c5968
Noah Tegeder <noah.tegeder@gmail.com>
2025-01-21 12:31:21 -0700

    Added heartbeat LED to main

--- app/main.asm
+++ app/main.asm
@@ -20,8 +20,22 @@ init:
             ; stop watchdog timer
             mov.w   #WDTPW+WDTHOLD,&WDTCTL
 
+            bic.b   #BIT0,&P1OUT            ; Clear P1.0 output
+            bis.b   #BIT0,&P1DIR            ; P1.0 output
+
+            bis.w   #TBCLR, &TB0CTL         ; Clear timers and dividers
+            bis.w   #TBSSEL__ACLK, &TB0CTL  ; ACLK as Timer source
+            bis.w   #MC__UP, &TB0CTL        ; Up counting mode 
+            mov.w   #32800, &TB0CCR0        ; initialize CCR0
+            bis.w   #CCIE, &TB0CCTL0         ; Enable capture/compare Interrupt
+            bis.w   #CCIFG, &TB0CCTL0        ; Clear interrupt flag
+            
+
             ; Disable low-power mode
             bic.w   #LOCKLPM5,&PM5CTL0
+            NOP
+            bis.w   #GIE, SR                ; Enable maskable interrupts
+            NOP
 
 main:
 
@@ -30,9 +44,22 @@ main:
             nop
 
 
+;------------------------------------------------------------------------------
+;           Interrupt Service Routines
+;------------------------------------------------------------------------------
+ISR_TB0_CCR0:
+            xor.b   #BIT0,&P1OUT              ; Toggle P6.6
+            bic.w   #CCIFG, &TB0CCTL0         ; Clear interrupt flag
+            reti
 
 ;------------------------------------------------------------------------------
 ;           Interrupt Vectors
 ;------------------------------------------------------------------------------
             .sect   RESET_VECTOR            ; MSP430 RESET Vector
             .short  RESET                   ;
+
+            .sect   ".int43"
+            .short  ISR_TB0_CCR0
+            .end
+
+
