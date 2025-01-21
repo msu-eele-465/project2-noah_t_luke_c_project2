b66af284de9d616cfdc1668901e76d6f39f11e28
luke connolly <ltconnolly8@gmail.com>
2025-01-21 12:33:46 -0700

    Heartbeat led for project 2

--- app/main.asm
+++ app/main.asm
@@ -14,14 +14,29 @@
             .retainrefs
 
 RESET       mov.w   #__STACK_END,SP         ; Initialize stack pointer
+STOPWDT     mov.w   #WDTPW+WDTHOLD,&WDTCTL  ; Stop WDT
 
+SetupP2     bic.b   #BIT6,&P6OUT            ; Clear P6.6 output
+            bis.b   #BIT6,&P6DIR            ; P6.6 output      
 
-init:
-            ; stop watchdog timer
-            mov.w   #WDTPW+WDTHOLD,&WDTCTL
+SetupTimer  bis.w   #TBCLR, &TB0CCTL0       ; Clear timer and dividers
+            bis.w   #TBSSEL__ACLK, &TB0CTL  ; ACLK as timer source
+            bis.w   #MC__UP, &TB0CTL        ; Up counting mode for timer
+            
+            mov.w   #32800, TB0CCR0
+            bis.w   #CCIE, &TB0CCTL0
+            bis.w   #CCIFG, &TB0CCTL0       ; Clear interrupt flag
+            
+            NOP
+            bis.w   #GIE, SR                ; Enable maskable interrupts
+            NOP
 
-            ; Disable low-power mode
-            bic.w   #LOCKLPM5,&PM5CTL0
+            bis.w #UCB0CTLW0, &UCSWRST      ; UCSWRST = 1 for eUSCI_B0 in SW reset
+            
+
+            bic.w   #LOCKLPM5,&PM5CTL0      ; Disable low-power mode
+
+            
 
 main:
 
@@ -33,6 +48,20 @@ main:
 
 ;------------------------------------------------------------------------------
 ;           Interrupt Vectors
+;------------------------------------------------------------------------------        
+ISR_TB0_CCR0:
+            xor.b   #BIT6, &P6OUT           ; Toggle P6.6
+            bic.w   #CCIFG, &TB0CCTL0       ; Clear flag
+            reti
+
+
+;------------------------------------------------------------------------------
+;           Interrupt Vectors
 ;------------------------------------------------------------------------------
             .sect   RESET_VECTOR            ; MSP430 RESET Vector
             .short  RESET                   ;
+
+            .sect ".int43"
+            .short ISR_TB0_CCR0
+
+            .end                ;
