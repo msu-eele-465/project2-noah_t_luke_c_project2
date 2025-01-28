6033bc29f6253ec3d9767a37e32e0d00fd994107
Noah Tegeder <noah.tegeder@gmail.com>
2025-01-27 22:31:09 -0700

    Added various subroutines
    implemented i2c_start and stop
    implemented rudimentary write command with address transmission

--- app/main.asm
+++ app/main.asm
@@ -17,13 +17,13 @@ RESET       mov.w   #__STACK_END,SP         ; Initialize stack pointer
 
 STOPWDT     mov.w   #WDTPW+WDTHOLD,&WDTCTL  ; Stop WDT
 
-SetupRED    bic.b   #BIT0,&P1OUT            ; Clear P6.6 output
-            bis.b   #BIT0,&P1DIR            ; P6.6 output    
+SetupRED    bic.b   #BIT0,&P1OUT            ; Clear P1.1 output
+            bis.b   #BIT0,&P1DIR            ; P1.1 output    
 
-SetupP13    bic.b   #BIT6,&P1OUT            ; Set P1.6 as SCL
+SetupP16    bic.b   #BIT6,&P1OUT            ; Set P1.6 as SCL
             bis.b   #BIT6,&P1DIR
 
-SetupP12    bic.b   #BIT5,&P1OUT            ; Set P1.5 as SDA
+SetupP15    bic.b   #BIT5,&P1OUT            ; Set P1.5 as SDA
             bis.b   #BIT5,&P1DIR
 
 SetupTimer  bis.w   #TBCLR, &TB0CCTL0       ; Clear timer and dividers
@@ -36,6 +36,7 @@ SetupTimer  bis.w   #TBCLR, &TB0CCTL0       ; Clear timer and dividers
             NOP
             bis.w   #GIE, SR                ; Enable maskable interrupts
             NOP
+            clrc                            ; Clear carry
 
 
 
@@ -44,20 +45,99 @@ DisableLPM  bic.w   #LOCKLPM5,&PM5CTL0      ; Disable low-power mode
            
 
 main:
-
-            nop
+            call    #i2c_start
+            call    #i2c_tx_byte
+            call    #i2c_stop
             jmp main
             nop
+
+;------------------------------------------------------------------------------
+;           Subroutines
+;------------------------------------------------------------------------------
+i2c_write:
+            call    #i2c_start
+            call    #i2c_tx_byte
+            call    #i2c_sda_delay
+            bic.b   #BIT5, &P1OUT           ; Clear SDA for WRITE
+            bis.b   #BIT6, &P1OUT           ; Pulse SCL
+            call    #i2c_sda_delay
+            bic.b   #BIT6, &P1OUT           
+            ret
+
+
+
+i2c_start:                                  ; Start signal, high to low on SDA, high on SCL
+            bis.b   #BIT6, &P1OUT           ; Set SCL high   
+            call    #i2c_sda_delay          ; Delay
+            bic.b   #BIT5, &P1OUT           ; Set SDA low
+            ret
+
+i2c_stop:                                   ; Stop signal, low to high on SDA, high on SCL, assume SCL high SDA low
+            bis.b   #BIT6, &P1OUT           ; Set SCL high   
+            call    #i2c_sda_delay          ; Delay
+            bis.b   #BIT5, &P1OUT           ; Set SDA high
+            ret
+
+i2c_tx_byte:                                ; Use to send a byte
+            mov.w  #8, R5                   ; Loop Counter
+
+tx_msb_tester:                              ; Test the MSB of tx_byte
+            bit.b  #BIT7, &tx_byte          ; Test bit 7
+            jz     clear_sda                ; If 0 clear SDA
+            bis.b  #BIT5, &P1OUT            ; If 1 set SDA
+            jmp    set_scl
+
+clear_sda:
+            bic.b  #BIT5, &P1OUT            ; Clear SDA
+
+set_scl:    
+            ; SDA Setup
+            bis.b  #BIT6, &P1OUT            ; SCL high
+            call   #i2c_sda_delay
+            bic.b  #BIT6, &P1OUT            ; SCL low
+            rlc.b  tx_byte                  ; Shift MSB
+            dec    R5                       ; Dec loop Counter
+            jnz    tx_msb_tester            ; loop
+            rlc.b  tx_byte                  ; Shift MSB
+            ret
+
+i2c_rx_ack:                                 ; Acknowledge data recieved
+            bic.b  #BIT5, &P1DIR            ; Input for SDA
+            nop
+
+            bis.b  #BIT6, &P1OUT            ; Set SCL high
+            nop
+            bit.b  #BIT5, &P1DIR            ; Check SDA with Z flag
+            bic.b  #BIT6, &P1OUT            ; SCL low
+           
+            bic.b  #BIT5, &P1DIR            ; Reset SDA to output
+            ret
+
+i2c_sda_delay:
+            nop
+            nop
+            ret
+
+
+i2c_scl_delay:
+
 
 
 
 ;------------------------------------------------------------------------------
-;           Interrupt Vectors
+;           Variables
+;------------------------------------------------------------------------------
+
+            .DATA
+tx_byte:    .byte  0x088;
+
+
+
+;------------------------------------------------------------------------------
+;           Interrupt Service Routines
 ;------------------------------------------------------------------------------        
 ISR_TB0_CCR0:
             xor.b   #BIT0, &P1OUT           ; Toggle P1.1
-            xor.b   #BIT6, &P1OUT
-            xor.b   #BIT5, &P1OUT
             bic.w   #CCIFG, &TB0CCTL0       ; Clear flag
             reti
 
